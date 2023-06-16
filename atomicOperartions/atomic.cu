3574045e61daedf747826072501b6bb183c71a5d
Soumil Datta <soumildatta@gmail.com>
2023-06-15 20:33:47 -0500

    copying result var

---
+++ atomicOperartions/atomic.cu
@@ -0,0 +1,27 @@
+#include <iostream>
+using std::cout;
+using std::endl;
+
+int main()
+{
+    size_t size = 500000000;
+
+    //! HOST DATA INITIALIZATION
+    // Allocate array on CPU
+    float *h_array = (float *)malloc(size);
+    // Add values to array on CPU
+    for(int i = 0; i < size; ++i)
+    {
+        h_array[i] = (float)(rand()) / (float)(rand());
+    }
+
+    //! DEVICE INITIALIZATION
+    float *d_array = NULL;
+    cudaMalloc((void **)&d_array, size);
+    cudaMemcpy(d_array, h_array, size, cudaMemcpyHostToDevice);
+
+    float result = 0.0f;
+    cudaMemcpy(result, sizeof(float), cudaMemcpyHostToDevice);
+
+    return 0;
+}
