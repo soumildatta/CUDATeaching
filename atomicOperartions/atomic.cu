c707e912b7d81f21e52b43280557abf41e68c638
Soumil Datta <soumildatta@gmail.com>
2023-06-16 07:35:38 -0500

    Atomic example

--- atomicOperartions/atomic.cu
+++ atomicOperartions/atomic.cu
@@ -2,30 +2,28 @@
 using std::cout;
 using std::endl;
 
-int main()
-{
-    size_t size = 500000000;
+__global__ void incrementVariable(int* result) {
+    atomicAdd(result, 1);
+}
 
-    //! HOST DATA INITIALIZATION
-    // Allocate array on CPU
-    float *h_array = (float *)malloc(size);
-    // Add values to array on CPU
-    for(int i = 0; i < size; ++i)
-    {
-        h_array[i] = (float)(rand()) / (float)(rand());
-    }
+int main() {
 
-    //! DEVICE INITIALIZATION
-    float *d_array = NULL;
-    cudaMalloc((void **)&d_array, size);
-    cudaMemcpy(d_array, h_array, size, cudaMemcpyHostToDevice);
+    int hostResult = 0;
+    int* deviceResult;
+    cudaMalloc((void**)&deviceResult, sizeof(int));
+    cudaMemcpy(deviceResult, &hostResult, sizeof(int), cudaMemcpyHostToDevice);
 
-    float *result = (float *)malloc(sizeof(float));
-    result[0] = 0;
+    // Launch the kernel
+    int numThreads = 256;
+    incrementVariable<<<1, numThreads>>>(deviceResult);
 
-    float *d_result = NULL;
-    cudaMalloc((void **)&d_result, sizeof(float));
-    cudaMemcpy(d_result, result, sizeof(float), cudaMemcpyHostToDevice);
+    // Copy the result back to the host
+    cudaMemcpy(&hostResult, deviceResult, sizeof(int), cudaMemcpyDeviceToHost);
+
+    cudaFree(deviceResult);
+
+    // Print the result
+    cout << "Final value: " << hostResult << endl;
 
     return 0;
 }
